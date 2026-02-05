2a25272801ba40ef0c021670d6404874c01efcaa
labbott <liam@bedrock.bio>
2026-02-05 15:37:17 -0500

    set up R package

---
+++ r/R/zzz.R
@@ -0,0 +1,9 @@
+.onLoad <- function(libname, pkgname) {
+  conn <- DBI::dbConnect(duckdb::duckdb())
+  on.exit(DBI::dbDisconnect(conn))
+  
+  tryCatch({
+    DBI::dbExecute(conn, "INSTALL httpfs")
+  }, error = function(e) {
+  })
+}
